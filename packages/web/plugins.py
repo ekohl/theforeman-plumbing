f03c72a9cd670e1604cf61819b21f4bdae3d5bf2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-05-10 16:16:59 +0200

    Add upstream tracking

--- packages/web/plugins.py
+++ packages/web/plugins.py
@@ -3,9 +3,16 @@ import os
 
 import requests
 
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify
 
 app = Flask(__name__)
+
+
+PREFIXES = {
+    'tfm-rubygem-': 'rubygem',
+    'rubygem-': 'rubygem',
+    'nodejs-': 'nodejs',
+}
 
 
 @app.route('/')
@@ -29,3 +36,33 @@ def release(rel, dist):
     results = {pkg: {'repo': data['version'], 'git': git.get(pkg)}
                for pkg, data in response.json().items() if not pkg.endswith('-doc')}
     return jsonify(results)
+
+
+@app.route('/upstream/<package>')
+def upstream(package):
+    for prefix, package_type in PREFIXES.items():
+        if package.startswith(prefix):
+            name = package[len(prefix):]
+            break
+    else:
+        # TODO: integrate release-monitoring.org?
+        package_type = None
+
+    if package_type == 'rubygem':
+        url = 'https://rubygems.org/api/v1/gems/{}.json'.format(name)
+        def get_version(data):
+            return data['version']
+    elif package_type == 'nodejs':
+        url = 'https://api.npms.io/v2/package/{}'.format(name)
+        def get_version(data):
+            return data['collected']['metadata']['version']
+    else:
+        abort(400)
+
+    response = requests.get(url)
+    if response.status_code == 404:
+        abort(404)
+    response.raise_for_status()
+    version = get_version(response.json())
+
+    return jsonify(version)
