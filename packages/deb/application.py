ekohl/theforeman-plumbing

commit 15ac52bed42a23d98d488c99a4353da3ec278323

Return a 404 if the URL was not found

packages/deb/application.py
@@ -4,7 +4,7 @@ import zlib
 
 import requests
 
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify
 
 app = Flask(__name__)
 
@@ -30,6 +30,8 @@ def get_packages(lines):
 @app.route('/<distribution>/<version>/<arch>')
 def packages(distribution, version, arch='amd64'):
     response = requests.get(BASEURL.format(distribution=distribution, version=version, arch=arch))
+    if response.status == 404:
+        abort(404)
     response.raise_for_status()
 
     data = zlib.decompress(response.content, zlib.MAX_WBITS | 32)

commit 303b56130851471384215b8376d8204c2b0a91c6

Detect the correct status code
A regression introduced in 15ac52bed42a23d98d488c99a4353da3ec278323

packages/deb/application.py
@@ -30,7 +30,7 @@ def get_packages(lines):
 @app.route('/<distribution>/<version>/<arch>')
 def packages(distribution, version, arch='amd64'):
     response = requests.get(BASEURL.format(distribution=distribution, version=version, arch=arch))
-    if response.status == 404:
+    if response.status_code == 404:
         abort(404)
     response.raise_for_status()
 